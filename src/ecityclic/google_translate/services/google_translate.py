1f39d60192b6c36a94eb14f28d9ffe3fd24b5771
Your Name <you@example.com>
2024-07-04 09:10:25 +0200

    Improve translation service for dicts

--- src/ecityclic/google_translate/services/google_translate.py
+++ src/ecityclic/google_translate/services/google_translate.py
@@ -13,6 +13,8 @@ class GoogleTranslatePost(Service):
     """ GoogleTranslatePost
     """
 
+    URL = 'https://www.googleapis.com/language/translate/v2'
+
     def reply(self):
         data = json_body(self.request)
         translate_text = data.get('translate_text', None)
@@ -21,29 +23,40 @@ class GoogleTranslatePost(Service):
         if not all([translate_text, source_lang, target_lang]):
             raise BadRequest("Missing required params: translated_text, source_lang, target_lang")
 
-        URL = 'https://www.googleapis.com/language/translate/v2'
-        key = api.portal.get_registry_record('plone.google_translation_key')
+        API_KEY = api.portal.get_registry_record('plone.google_translation_key')
+        translated = self.translate(API_KEY, source_lang, target_lang, translate_text)
+        return {'translated_text': translated}
+
+    def translate(self, API_KEY, source_lang, target_lang, translate_text):
+        if isinstance(translate_text, list):
+            return self.translate_list(API_KEY, source_lang, target_lang, translate_text)
+
         data = {
-            'key': key,
+            'key': API_KEY,
             'source': source_lang,
             'target': target_lang,
-            'q': json.dumps(translate_text) if isinstance(translate_text, list) else translate_text
+            'q': translate_text
         }
 
         data = urllib.parse.urlencode(data).encode()
-        req = urllib.request.Request(URL, data=data)
+        req = urllib.request.Request(self.URL, data=data)
         try:
             r = urllib.request.urlopen(req, timeout=10)
         except Exception as e:  # noqa
             return {'error': 'no_more_quota'}
 
         translated = json.loads(r.read())['data']['translations'][0]['translatedText']
-        if isinstance(translate_text, list):
-            translated = html.unescape(translated)
-            translated = translated.replace('" text"', '"text"').replace('"text "', '"text"')
-            translated = translated.replace('" children"', '"children"').replace('"children "', '"children"')
-            translated = json.loads(translated)
-        else:
-            translated = html.unescape(translated)
+        translated = html.unescape(translated)
+        return translated
 
-        return {'translated_text': translated}
+    def translate_list(self, API_KEY, source_lang, target_lang, translate_text):
+        for child in translate_text:
+            self.translate_dict(API_KEY, source_lang, target_lang, child)
+        return translate_text
+
+    def translate_dict(self, API_KEY, source_lang, target_lang, translate_text):
+        for key in translate_text:
+            if key == 'children':
+                self.translate_list(API_KEY, source_lang, target_lang, translate_text[key])
+            if key == 'text':
+                translate_text[key] = self.translate(API_KEY, source_lang, target_lang, translate_text[key])
